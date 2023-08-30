0aa0483fc14f9c29443ba4792c36fe73e6d292e8
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-08-30 18:27:28 +0000

    install webextension polyfill

--- apps/browser-extension/src/workers/extension-worker.ts
+++ apps/browser-extension/src/workers/extension-worker.ts
@@ -1,7 +1,11 @@
-chrome.action.onClicked.addListener(async () => {
+import browser from "webextension-polyfill";
+
+console.log(browser);
+
+browser.action.onClicked.addListener(async () => {
   const currentTab = await getCurrentTab();
 
-  const notebookMutableUrl = new URL(chrome.runtime.getURL("notebook.html"));
+  const notebookMutableUrl = new URL(browser.runtime.getURL("options.html"));
   if (currentTab?.title) {
     notebookMutableUrl.searchParams.set("title", currentTab.title);
   }
@@ -9,13 +13,13 @@ chrome.action.onClicked.addListener(async () => {
     notebookMutableUrl.searchParams.set("url", currentTab.url);
   }
 
-  chrome.tabs.create({ url: notebookMutableUrl.toString() });
+  browser.tabs.create({ url: notebookMutableUrl.toString() });
 });
 
 // ref: https://developer.chrome.com/docs/extensions/reference/tabs/#get-the-current-tab
 async function getCurrentTab() {
   let queryOptions = { active: true, lastFocusedWindow: true };
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
+  let [tab] = await browser.tabs.query(queryOptions);
   return tab;
 }
