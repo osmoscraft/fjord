0151afaf86d2000f4043358af0862b75aadc675f
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-09-04 21:16:00 +0000

    handle merge

--- apps/browser-extension/src/workers/extension-worker.ts
+++ apps/browser-extension/src/workers/extension-worker.ts
@@ -15,22 +15,73 @@ browser.runtime.onMessage.addListener(async (message: MessageToExtensionWorker) 
     // ensure root folder exists
     const existingRootFolder = await browser.bookmarks.search({
       title: "Fjord",
-      url: "",
     });
 
     console.log("search", existingRootFolder);
   }
 
   if (message.didFetchFeed) {
+    // TODO implement declarative merge: compute latest value, then resolve difference
+
     const feed = message.didFetchFeed;
     const rootFolder = await browser.bookmarks.create({ title: "Fjord", parentId: "1", index: 0 }); // HACK: 1 is the "Favorites bar"
-    const feedFolder = await browser.bookmarks.create({ title: feed.title, parentId: rootFolder.id });
-    console.log("fetched", { feed, rootFolder, feedFolder });
 
-    Promise.all(
-      feed.items
-        .slice(0, 10)
-        .map((item, index) => browser.bookmarks.create({ title: item.title, url: item.url, parentId: feedFolder.id }))
+    const existingChannels = (await browser.bookmarks.getSubTree(rootFolder.id))[0]?.children ?? [];
+    const existingChannel = existingChannels.find(
+      (node) => !node.url && (node.title === feed.title || undecorateFeedTitle(node.title) === feed.title)
     );
+
+    const channelFolder =
+      existingChannel ?? (await browser.bookmarks.create({ title: feed.title, parentId: rootFolder.id }));
+    const existingItems = (await browser.bookmarks.getSubTree(channelFolder.id))[0]?.children ?? [];
+
+    const sortedNewItems = feed.items.sort((a, b) => a.timePublished - b.timePublished).slice(-10);
+
+    for (const newItem of sortedNewItems) {
+      const existingItem = existingItems.find((existingItem) => existingItem.url === newItem.url);
+
+      // update existing item
+      if (existingItem) {
+        await browser.bookmarks.update(existingItem.id, {
+          title: setItemTitleIsUnread(newItem.title, isItemTitleUnread(existingItem.title)),
+          url: newItem.url,
+        });
+      } else {
+        await browser.bookmarks.create({
+          title: setItemTitleIsUnread(newItem.title, !!existingChannel),
+          index: 0,
+          url: newItem.url,
+          parentId: channelFolder.id,
+        });
+      }
+    }
+
+    const allChannelItems = (await browser.bookmarks.getSubTree(channelFolder.id))[0]?.children ?? [];
+    const remainingItems = allChannelItems.slice(0, 10);
+    const removeItems = allChannelItems.slice(10);
+    await Promise.all(removeItems.map((item) => browser.bookmarks.remove(item.id)));
+
+    const newCount = remainingItems.reduce((count, item) => {
+      return count + (isItemTitleUnread(item.title) ? 1 : 0);
+    }, 0);
+
+    browser.bookmarks.update(channelFolder.id, { title: newCount ? `(${newCount}) ${feed.title}` : feed.title });
   }
 });
+
+function undecorateFeedTitle(title: string) {
+  if (title.match(/^\(\d+\)\s.*/)) {
+    return title.replace(/^\(\d+\)\s/, "");
+  } else {
+    return title;
+  }
+}
+
+function isItemTitleUnread(title: string) {
+  return title.startsWith("• ");
+}
+
+function setItemTitleIsUnread(title: string, isUnread: boolean) {
+  const undecoratedTitel = title.startsWith("• ") ? title.slice(2) : title;
+  return isUnread ? `• ${undecoratedTitel}` : undecoratedTitel;
+}
