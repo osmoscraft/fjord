65380a52cb7f061b2324700f49bf4bca183b0613
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-09-04 21:46:25 +0000

    track read receipt

--- apps/browser-extension/src/workers/extension-worker.ts
+++ apps/browser-extension/src/workers/extension-worker.ts
@@ -10,6 +10,41 @@ if (preference.runOnStartUp) {
   browser.runtime.onStartup.addListener(() => setupOffscreenDocument(backgroundPageParameters));
 }
 
+// update status on read
+browser.tabs.onUpdated.addListener(async (id, info) => {
+  if (!info.url) return;
+
+  const existingRootFolder = await browser.bookmarks.search({
+    title: "Fjord",
+  });
+  const channels = (await browser.bookmarks.getSubTree(existingRootFolder[0].id))[0]?.children ?? [];
+
+  browser.tabs.get(id).then(async (tab) => {
+    const matchedItems = (await browser.bookmarks.search({ url: info.url })).filter((maybeItem) =>
+      channels.some((channel) => channel.id === maybeItem.parentId)
+    );
+
+    for (const item of matchedItems) {
+      await browser.bookmarks.update(item.id, {
+        title: setItemTitleIsUnread(item.title, false),
+      });
+    }
+
+    const parentChannel = channels.find((channel) => channel.id === matchedItems[0].parentId);
+    if (!parentChannel) return;
+
+    const channelItems = (await browser.bookmarks.getSubTree(parentChannel.id))[0]?.children ?? [];
+    const newCount = channelItems.reduce((count, item) => {
+      return count + (isItemTitleUnread(item.title) ? 1 : 0);
+    }, 0);
+
+    await browser.bookmarks.update(parentChannel.id, {
+      title: setChannelTitle(parentChannel.title, newCount),
+    });
+  });
+});
+
+// merge bookmarks
 browser.runtime.onMessage.addListener(async (message: MessageToExtensionWorker) => {
   if (message.willFetchAllFeeds) {
     // ensure root folder exists
@@ -28,7 +63,7 @@ browser.runtime.onMessage.addListener(async (message: MessageToExtensionWorker) 
 
     const existingChannels = (await browser.bookmarks.getSubTree(rootFolder.id))[0]?.children ?? [];
     const existingChannel = existingChannels.find(
-      (node) => !node.url && (node.title === feed.title || undecorateFeedTitle(node.title) === feed.title)
+      (node) => !node.url && (node.title === feed.title || undecorateChannelTitle(node.title) === feed.title)
     );
 
     const channelFolder =
@@ -69,19 +104,24 @@ browser.runtime.onMessage.addListener(async (message: MessageToExtensionWorker) 
   }
 });
 
-function undecorateFeedTitle(title: string) {
-  if (title.match(/^\(\d+\)\s.*/)) {
-    return title.replace(/^\(\d+\)\s/, "");
-  } else {
-    return title;
-  }
-}
-
 function isItemTitleUnread(title: string) {
   return title.startsWith("• ");
 }
 
 function setItemTitleIsUnread(title: string, isUnread: boolean) {
-  const undecoratedTitel = title.startsWith("• ") ? title.slice(2) : title;
-  return isUnread ? `• ${undecoratedTitel}` : undecoratedTitel;
+  const undecoratedTitle = title.startsWith("• ") ? title.slice(2) : title;
+  return isUnread ? `• ${undecoratedTitle}` : undecoratedTitle;
+}
+
+function setChannelTitle(title: string, newCount: number) {
+  const undecoratedTitle = undecorateChannelTitle(title);
+  return newCount ? `(${newCount}) ${undecoratedTitle}` : undecoratedTitle;
+}
+
+function undecorateChannelTitle(title: string) {
+  if (title.match(/^\(\d+\)\s.*/)) {
+    return title.replace(/^\(\d+\)\s/, "");
+  } else {
+    return title;
+  }
 }
