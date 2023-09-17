f4ddba3e1e7dbfa2f9669f42c92542a07d62db53
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-09-17 17:00:28 +0000

    refactor worker

--- apps/browser-extension/src/workers/extension-worker.ts
+++ apps/browser-extension/src/workers/extension-worker.ts
@@ -1,49 +1,26 @@
 import browser from "webextension-polyfill";
-import { getUnreadUrls, markAsSeen } from "../modules/bookmarks";
 import { setupOffscreenDocument } from "../modules/offscreen";
 import { backgroundPageParameters } from "../modules/parameters";
-import type { ExtensionMessage } from "../typings/events";
 
-browser.runtime.onInstalled.addListener(() => setupOffscreenDocument(backgroundPageParameters));
-browser.runtime.onStartup.addListener(() => setupOffscreenDocument(backgroundPageParameters));
-browser.runtime.onMessage.addListener(async (message: ExtensionMessage) => {
-  if (message.requestUnreadUrls) {
-    emitUnseenUrls();
-  }
+browser.runtime.onInstalled.addListener(handleExtensionInstall);
+browser.runtime.onStartup.addListener(handleBrowserStart);
+browser.runtime.onMessage.addListener(handleExtensionWorkerMessage);
+browser.bookmarks.onChanged.addListener(handleBookmarksChange);
+browser.bookmarks.onRemoved.addListener(handleBookmarksChange);
+browser.bookmarks.onCreated.addListener(handleBookmarksChange);
+browser.bookmarks.onMoved.addListener(handleBookmarksChange);
+browser.history.onVisited.addListener(handleVisit);
 
-  if (message.channelUnreadItems) {
-    const root = await browser.bookmarks.create({
-      title: "Feed",
-      parentId: "1",
-    });
-
-    const channelFolder = await browser.bookmarks.create({
-      title: message.channelUnreadItems.channelUrl,
-      parentId: root.id,
-    });
-
-    message.channelUnreadItems.items.map((item) =>
-      browser.bookmarks.create({
-        title: item.title,
-        url: item.url,
-        parentId: channelFolder.id,
-      })
-    );
-  }
-});
-
-browser.bookmarks.onChanged.addListener(emitUnseenUrls);
-browser.bookmarks.onRemoved.addListener(emitUnseenUrls);
-browser.bookmarks.onCreated.addListener(emitUnseenUrls);
-browser.bookmarks.onMoved.addListener(emitUnseenUrls);
-
-browser.history.onVisited.addListener(async (result) => {
-  if (!result.url) return;
-  markAsSeen(result.url);
-});
-
-async function emitUnseenUrls() {
-  const unreadUrls = await getUnreadUrls();
-  console.log("unreadUrls changed", { unreadUrls });
-  browser.runtime.sendMessage({ unreadUrls } satisfies ExtensionMessage);
+function handleExtensionInstall() {
+  return setupOffscreenDocument(backgroundPageParameters);
 }
+
+function handleBrowserStart() {
+  return setupOffscreenDocument(backgroundPageParameters);
+}
+
+function handleBookmarksChange() {}
+
+function handleExtensionWorkerMessage(message: any, sender: browser.Runtime.MessageSender, sendResponse: () => void) {}
+
+function handleVisit(result: browser.History.HistoryItem) {}
