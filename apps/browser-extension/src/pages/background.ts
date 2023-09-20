d76e3ed949dfa97bba884eaa3cc7000f72812d19
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-09-20 06:48:54 +0000

    e2e prototype

--- apps/browser-extension/src/pages/background.ts
+++ apps/browser-extension/src/pages/background.ts
@@ -1,15 +1,27 @@
 import browser from "webextension-polyfill";
+import { getRawConfig, parseConfig } from "../modules/config/config";
 import { parseXmlFeed } from "../modules/feed-parser/parse";
 import type { ExtensionMessage } from "../typings/message";
 
 browser.runtime.onMessage.addListener(async (message: ExtensionMessage) => {
   if (message.fetchAll) {
-    const xml = await fetch("https://css-tricks.com/feed/").then((res) => res.text());
-    const parsedFeed = parseXmlFeed(xml);
-    console.log("background parsed", parsedFeed);
+    const rawConfig = getRawConfig();
+    if (!rawConfig) throw new Error("missing config");
 
-    await browser.runtime.sendMessage({
-      channelData: { ...parsedFeed, url: "https://css-tricks.com/feed/" },
-    } satisfies ExtensionMessage);
+    const config = parseConfig(rawConfig);
+
+    config.channels.map(async (channel) => {
+      await fetch(channel.url)
+        .then((res) => res.text())
+        .then((xml) => {
+          const parsedFeed = parseXmlFeed(xml);
+          console.log("[background] parsed", parsedFeed);
+
+          browser.runtime.sendMessage({
+            channelData: { ...parsedFeed, url: channel.url },
+          } satisfies ExtensionMessage);
+        })
+        .catch((e) => console.log("[background] parse error", { channel }));
+    });
   }
 });
