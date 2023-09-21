4d07baa200c3c09a9aea5b7c0ccfabd546ff3923
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-09-21 06:00:30 +0000

    truncate items

--- apps/browser-extension/src/modules/bookmarks.ts
+++ apps/browser-extension/src/modules/bookmarks.ts
@@ -1,5 +1,6 @@
 import browser from "webextension-polyfill";
 import { dataUrlToObject, objectToDataUrl } from "./codec";
+import type { FeedItem } from "./feed-parser/types";
 import type { ChannelData, ChannelDataWithUnreadUrls } from "./reader/render-feed";
 
 export async function getChannels(): Promise<ChannelDataWithUnreadUrls[]> {
@@ -25,15 +26,18 @@ export async function setChannelBookmark(channelData: ChannelData) {
 
     const existingChannelData = await dataUrlToObject<ChannelDataWithUnreadUrls>(existingBookmark.url!);
     const mergedChannelData = mergeBookmark(channelData, existingChannelData, unreadUrls);
-
-    const dataUrl = await objectToDataUrl(mergedChannelData);
+    const dataUrl = await objectToDataUrl({ ...mergedChannelData, items: filterItems(mergedChannelData.items) });
     await browser.bookmarks.update(existingBookmark.id, {
       url: dataUrl,
     });
   } else {
     // when creating a channel, consider all urls as read
 
-    const dataUrl = await objectToDataUrl({ ...channelData, unreadUrls: [] } satisfies ChannelDataWithUnreadUrls);
+    const dataUrl = await objectToDataUrl({
+      ...channelData,
+      items: filterItems(channelData.items),
+      unreadUrls: [],
+    } satisfies ChannelDataWithUnreadUrls);
     await browser.bookmarks.create({
       title: channelData.url,
       url: dataUrl,
@@ -93,13 +97,18 @@ function mergeBookmark(
 
   const mergedChannelData: ChannelDataWithUnreadUrls = {
     ...incoming,
-    items: [...incoming.items, ...existing.items].filter(
-      (item, index, arr) => arr.findIndex((i) => i.url === item.url) === index
-    ),
+    items: [...incoming.items, ...existing.items],
     unreadUrls: [...newItems.map((item) => item.url), ...unreadUrls].filter((url) => channelItemUrls.has(url)),
   };
 
   return mergedChannelData;
+}
+
+function filterItems(feedItems: FeedItem[]): FeedItem[] {
+  return feedItems
+    .filter((item, index, arr) => arr.findIndex((i) => i.url === item.url) === index)
+    .filter((item) => item.timePublished > Date.now() - 1000 * 60 * 60 * 24 * 30) // 30 day old
+    .slice(0, 20); // 20 items per channel
 }
 
 async function getRoot() {
