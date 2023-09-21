cf2ff00c9447e69b6dd1a49454e9fd0c2407b8c6
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-09-21 06:38:04 +0000

    fix truncation issue

--- apps/browser-extension/src/modules/bookmarks.ts
+++ apps/browser-extension/src/modules/bookmarks.ts
@@ -1,6 +1,5 @@
 import browser from "webextension-polyfill";
 import { dataUrlToObject, objectToDataUrl } from "./codec";
-import type { FeedItem } from "./feed-parser/types";
 import type { ChannelData, ChannelDataWithUnreadUrls } from "./reader/render-feed";
 
 export async function getChannels(): Promise<ChannelDataWithUnreadUrls[]> {
@@ -23,21 +22,21 @@ export async function setChannelBookmark(channelData: ChannelData) {
   if (existingBookmark) {
     // new items to be marked as unread
     const unreadUrls = await getAllUnreadUrls();
-
     const existingChannelData = await dataUrlToObject<ChannelDataWithUnreadUrls>(existingBookmark.url!);
     const mergedChannelData = mergeBookmark(channelData, existingChannelData, unreadUrls);
-    const dataUrl = await objectToDataUrl({ ...mergedChannelData, items: filterItems(mergedChannelData.items) });
+    const dataUrl = await objectToDataUrl(truncateItems(mergedChannelData));
     await browser.bookmarks.update(existingBookmark.id, {
       url: dataUrl,
     });
   } else {
     // when creating a channel, consider all urls as read
-
-    const dataUrl = await objectToDataUrl({
-      ...channelData,
-      items: filterItems(channelData.items),
-      unreadUrls: [],
-    } satisfies ChannelDataWithUnreadUrls);
+    const dataUrl = await objectToDataUrl(
+      truncateItems({
+        ...channelData,
+        items: channelData.items,
+        unreadUrls: [],
+      } satisfies ChannelDataWithUnreadUrls)
+    );
     await browser.bookmarks.create({
       title: channelData.url,
       url: dataUrl,
@@ -114,11 +113,18 @@ function mergeBookmark(
   return mergedChannelData;
 }
 
-function filterItems(feedItems: FeedItem[]): FeedItem[] {
-  return feedItems
+function truncateItems(channelData: ChannelDataWithUnreadUrls): ChannelDataWithUnreadUrls {
+  const remainingItems = channelData.items
     .filter((item, index, arr) => arr.findIndex((i) => i.url === item.url) === index)
     .filter((item) => item.timePublished > Date.now() - 1000 * 60 * 60 * 24 * 30) // 30 day old
     .slice(0, 20); // 20 items per channel
+  const remainingUnreadUrls = channelData.unreadUrls.filter((url) => remainingItems.some((item) => item.url === url));
+
+  return {
+    ...channelData,
+    items: remainingItems,
+    unreadUrls: remainingUnreadUrls,
+  };
 }
 
 async function getRoot() {
