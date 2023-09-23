4cee5c172a90cb3104b04811a54887600cf8ca26
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-09-23 20:50:26 +0000

    add floating menu

--- apps/browser-extension/src/modules/reader/render-command-bar.ts
+++ apps/browser-extension/src/modules/reader/render-command-bar.ts
@@ -1,5 +1,5 @@
 export function renderCommandBar() {
-  return /*html*/ `<menu>
+  return /*html*/ `<menu class="c-command-bar">
   <button data-action="fetch">Fetch</button>  
   <button data-action="mark-all-as-read">Mark all as read</button>
   <button data-action="options">Options</button>
